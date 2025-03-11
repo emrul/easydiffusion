31cdef3b8bd0bd36e2d4ea18cea96efe19279041
Emrul <emrul@emrul.com>
2025-03-11 12:35:26 +0000

    Update app.py

--- ui/easydiffusion/app.py
+++ ui/easydiffusion/app.py
@@ -35,7 +35,7 @@ SD_DIR = os.getcwd()
 ROOT_DIR = os.path.abspath(os.path.join(SD_DIR, ".."))
 
 SD_UI_DIR = os.getenv("SD_UI_PATH", None)
-
+log.info("SD_UI_DIR is '{SD_UI_DIR}'")
 CONFIG_DIR = os.path.abspath(os.path.join(SD_UI_DIR, "..", "scripts"))
 BUCKET_DIR = os.path.abspath(os.path.join(SD_DIR, "..", "bucket"))
 
